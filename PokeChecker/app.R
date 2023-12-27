5c0ae5858b7d650d8d7bb10f78e894c4924dc2d1
Peter-T-Ruehr <peter.ruehr@gmail.com>
2023-12-27 19:35:23 +0100

    add interactive google seet link

--- PokeChecker/app.R
+++ PokeChecker/app.R
@@ -5,14 +5,22 @@ gs4_deauth()
 library(DT)
 
 # load google sheets
-sheet_id <- "https://docs.google.com/spreadsheets/d/1WZWY-zCCki9jD26-v7QQ1CCX85NmiYclPLukeIPHJV4/"
-pokedex_df <- read_sheet(sheet_id, sheet = "pokedex")
-evolutions_df <- read_sheet(sheet_id, sheet = "evolutions")
-# pokedex_df <- tibble(mtcars) %>% 
-#   mutate(name = rownames(mtcars))
+pokedex_sheet_id <- "https://docs.google.com/spreadsheets/d/1WZWY-zCCki9jD26-v7QQ1CCX85NmiYclPLukeIPHJV4/"
+pokedex_df <- read_sheet(pokedex_sheet_id, sheet = "pokedex")
+
+evolutions_sheet_id <- "https://docs.google.com/spreadsheets/d/1WZWY-zCCki9jD26-v7QQ1CCX85NmiYclPLukeIPHJV4/"
+evolutions_df <- read_sheet(evolutions_sheet_id, sheet = "evolutions")
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  textInput(inputId = "pokedex_sheet_id", 
+            label = "Spreadsheet URL", 
+            value = "https://docs.google.com/spreadsheets/d/1WZWY-zCCki9jD26-v7QQ1CCX85NmiYclPLukeIPHJV4/", 
+            width = NULL, 
+            placeholder = NULL),
+  
   selectInput(inputId = "mon_of_interest_german", 
               label = "Pokemon", 
               choices = pokedex_df$name_german,
@@ -27,6 +35,10 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output, session) {
+  pokedex_df <- reactive({
+    read_sheet(input$pokedex_sheet_id, sheet = "pokedex")
+  })
+  
   output$data_present <- DT::renderDataTable({
     req(input$mon_of_interest_german)
     
@@ -39,12 +51,12 @@ server <- function(input, output, session) {
       mon_of_interest_german <- gsub(" .+$", "", mon_of_interest_german)
     }
     
-    mon_of_interest_english <- pokedex_df %>% 
+    mon_of_interest_english <- pokedex_df() %>% 
       mutate(name_german = gsub(" .+$", "", name_german)) %>% 
       filter(name_german == mon_of_interest_german) %>% 
       slice(1) %>% 
       pull(name_english) #%>% 
-      # gsub(" .+$", "", .)
+    # gsub(" .+$", "", .)
     
     if(grepl('\\(', mon_of_interest_english)){
       mon_of_interest_english <- gsub(" .+$", "", mon_of_interest_english)
@@ -60,7 +72,7 @@ server <- function(input, output, session) {
     
     
     # get entries of current stages
-    curr_pokedex_present <- pokedex_df %>% 
+    curr_pokedex_present <- pokedex_df() %>% 
       mutate(name_english = gsub(" \\(.+", "", name_english)) %>% 
       filter(name_english %in% stages)
     
@@ -192,141 +204,6 @@ server <- function(input, output, session) {
                                  dom = 'tB',
                                  autoWidth = TRUE,
                                  buttons = c('copy', 'csv', 'excel')))
-  })
-  
-  output$data_needed <- DT::renderDataTable({ # renderTable
-    req(input$mon_of_interest_german)
-    
-    mon_of_interest_german <- gsub(" .+$", "", input$mon_of_interest_german)
-    
-    mon_of_interest_english <- pokedex_df %>% 
-      mutate(name_german = gsub(" .+$", "", name_german)) %>% 
-      filter(name_german == mon_of_interest_german) %>% 
-      slice(1) %>% 
-      pull(name_english) %>% 
-      gsub(" .+$", "", .)
-    
-    stages <- evolutions_df %>% 
-      filter(name1 == mon_of_interest_english |
-               name2 == mon_of_interest_english |
-               name3 == mon_of_interest_english) %>% 
-      unlist()
-    
-    stages <- stages[!is.na(stages)]
-    
-    entries_present <- pokedex_df %>% 
-      filter(name_english %in% stages)
-    
-    
-    
-    highest_stage <- length(stages)
-    
-    needs_stage1 <- c("m1", "f1", "m1_3stars", "f1_3stars", "m1_shadow", "f1_shadow", "m1_purified", "f1_purified")
-    needs_stage2 <- c("m2", "f2", "m2_3stars", "f2_3stars", "m2_shadow", "f2_shadow", "m2_purified", "f2_purified")
-    needs_stage3 <- c("m3", "f3", "m3_3stars", "f3_3stars", "m3_shadow", "f3_shadow", "m2_purified", "f2_purified")
-    
-    if(highest_stage == 3){
-      needs <- tibble(needs_stage1, needs_stage2, needs_stage3)
-    } else if(highest_stage == 2){
-      needs <- tibble(needs_stage1, needs_stage2)
-    }  else if(highest_stage == 1){
-      needs <- tibble(needs_stage1)
-    } 
-    
-    # what is needed in 3rd stage?
-    if(length(stages) == 3){
-      stage_3_male_needed <- ifelse(entries_present$male_in_dex[3]=="x", FALSE, TRUE)
-      stage_3_female_needed <- ifelse(entries_present$female_in_dex[3]=="x", FALSE, TRUE)
-      stage_3_stars3_needed <- ifelse(entries_present$stars3_in_dex[3]=="x", FALSE, TRUE)
-      stage_3_shadow <- ifelse(entries_present$shadow_in_dex[3]=="x", FALSE, TRUE)
-      stage_3_purified_needed <- ifelse(entries_present$purified_in_dex[3]=="x", FALSE, TRUE)
-      
-      if(!is.na(stage_3_male_needed)){
-        needs$needs_stage3[1] <- NA
-      }
-      if(!is.na(stage_3_female_needed)){
-        needs$needs_stage3[2] <- NA
-      }
-      if(!is.na(stage_3_stars3_needed)){
-        needs$needs_stage3[3] <- NA
-        needs$needs_stage3[4] <- NA
-      }
-      if(!is.na(stage_3_shadow)){
-        needs$needs_stage3[5] <- NA
-        needs$needs_stage3[6] <- NA
-      }
-      if(!is.na(stage_3_purified_needed)){
-        needs$needs_stage3[7] <- NA
-        needs$needs_stage3[8] <- NA
-      }
-    }
-    
-    # what is needed in 2nd stage?
-    if(length(stages) >= 2){
-      stage_2_male_needed <- ifelse(entries_present$male_in_dex[2]=="x", FALSE, TRUE)
-      stage_2_female_needed <- ifelse(entries_present$female_in_dex[2]=="x", FALSE, TRUE)
-      stage_2_stars3_needed <- ifelse(entries_present$stars3_in_dex[2]=="x", FALSE, TRUE)
-      stage_2_shadow <- ifelse(entries_present$shadow_in_dex[2]=="x", FALSE, TRUE)
-      stage_2_purified_needed <- ifelse(entries_present$purified_in_dex[2]=="x", FALSE, TRUE)
-      
-      if(!is.na(stage_2_male_needed)){
-        needs$needs_stage2[1] <- NA
-      }
-      if(!is.na(stage_2_female_needed)){
-        needs$needs_stage2[2] <- NA
-      }
-      if(!is.na(stage_2_stars3_needed)){
-        needs$needs_stage2[3] <- NA
-        needs$needs_stage2[4] <- NA
-      }
-      if(!is.na(stage_2_shadow)){
-        needs$needs_stage2[5] <- NA
-        needs$needs_stage2[6] <- NA
-      }
-      if(!is.na(stage_2_purified_needed)){
-        needs$needs_stage2[7] <- NA
-        needs$needs_stage2[8] <- NA
-      }
-    }
-    
-    # what is needed in 1st stage?
-    stage_1_male_needed <- ifelse(entries_present$male_in_dex[1]=="x", FALSE, TRUE)
-    stage_1_female_needed <- ifelse(entries_present$female_in_dex[1]=="x", FALSE, TRUE)
-    stage_1_stars3_needed <- ifelse(entries_present$stars3_in_dex[1]=="x", FALSE, TRUE)
-    stage_1_shadow <- ifelse(entries_present$shadow_in_dex[1]=="x", FALSE, TRUE)
-    stage_1_purified_needed <- ifelse(entries_present$purified_in_dex[1]=="x", FALSE, TRUE)
-    
-    if(!is.na(stage_1_male_needed)){
-      needs$needs_stage1[1] <- NA
-    }
-    if(!is.na(stage_1_female_needed)){
-      needs$needs_stage1[2] <- NA
-    }
-    if(!is.na(stage_1_stars3_needed)){
-      needs$needs_stage1[3] <- NA
-      needs$needs_stage1[4] <- NA
-    }
-    if(!is.na(stage_1_shadow)){
-      needs$needs_stage1[5] <- NA
-      needs$needs_stage1[6] <- NA
-    }
-    if(!is.na(stage_1_purified_needed)){
-      needs$needs_stage1[7] <- NA
-      needs$needs_stage1[8] <- NA
-    }
-    
-    DT::datatable(needs,
-                  escape = FALSE,
-                  
-                  # extensions = 'Buttons',
-                  
-                  options = list(pageLength = 1000,
-                                 searching = FALSE,
-                                 lengthChange = FALSE,
-                                 dom = 'tB',
-                                 autoWidth = TRUE,
-                                 buttons = c('copy', 'csv', 'excel')))
-    
   })
 }
 
