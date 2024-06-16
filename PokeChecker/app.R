4288088e0f60569a96af893feb65f9d6cb3e30a9
Peter-T-Ruehr <peter.ruehr@gmail.com>
2024-06-16 18:31:42 +0200

    main update

--- PokeChecker/app.R
+++ PokeChecker/app.R
@@ -1,21 +1,46 @@
 library(shiny)
-library(dplyr)
 library(googlesheets4)
 gs4_deauth()
 library(DT)
+library(stringr) 
+library(data.table)
+library(tidyr)
+library(dplyr)
+
+# translations
+# translations_df_wide <- read_sheet(pokedex_sheet_id,sheet = "translations")
+# translations_df_wide <- read.csv(file = "PokeChecker/data/translations_df_wide.csv")
+translations_df_wide <- read.csv(file = "data/translations_df_wide.csv")
+# get rid of #
+translations_df_wide <- translations_df_wide %>% 
+  mutate(dex = gsub("#", "", dex))
+
+# evolution chains
+# evolutions_df <- read_sheet(pokedex_sheet_id,sheet = "evolution_chains") 
+# evolutions_df <- read.csv(file = "PokeChecker/data/evolution_chains.csv")
+evolutions_df <- read.csv(file = "data/evolution_chains.csv")
+# get rid of #
+evolutions_df <- evolutions_df %>%
+  mutate(across(everything(), ~ gsub("#", "", .)))
 
 # load google sheets
 pokedex_sheet_id <- "https://docs.google.com/spreadsheets/d/1WZWY-zCCki9jD26-v7QQ1CCX85NmiYclPLukeIPHJV4/"
-pokedex_df <- read_sheet(pokedex_sheet_id, sheet = "pokedex") %>% 
-  rename(German = name_german,
-         English = name_english)
 
-evolutions_sheet_id <- "https://docs.google.com/spreadsheets/d/1WZWY-zCCki9jD26-v7QQ1CCX85NmiYclPLukeIPHJV4/"
-evolutions_df <- read_sheet(evolutions_sheet_id, sheet = "evolutions")
+# pokedex
+pokedex_df_raw <- read_sheet(pokedex_sheet_id,sheet = "pokedex") 
 
 
 
-# Define UI for application that draws a histogram
+
+colnames(translations_df_wide) <- gsub("name_", "", colnames(translations_df_wide))
+
+translations_df <- as_tibble(melt(setDT(translations_df_wide), 
+                                  id.vars = c("dex"), 
+                                  variable.name = "language", 
+                                  value.name = "name"))
+
+
+# Define UI for application 
 ui <- fluidPage(
   textInput(inputId = "pokedex_sheet_id", 
             label = "Spreadsheet URL", 
@@ -26,8 +51,8 @@ ui <- fluidPage(
   radioButtons(
     inputId = "language",
     label = "Language",
-    choices = c("English", "German"), # "English", "German"
-    selected = "German",
+    choices = c("name_english", "name_german"), # "English", "German", "name_english", "name_german"
+    selected = "name_german",
     inline = FALSE,
     width = NULL,
     choiceNames = NULL,
@@ -37,38 +62,45 @@ ui <- fluidPage(
   
   selectInput(inputId = "mon_of_interest", 
               label = "Pokemon", 
-              choices = pokedex_df$German,
+              choices = pokedex_df_raw$name_german,
               multiple = FALSE),
   
   h3("Present in Pokédex"),
-  # textOutput("mon_of_interest_selected_orig"),
-  # textOutput("mon_of_interest_selected_final"),
+  textOutput("mon_of_interest_selected_orig"),
+  textOutput("mon_of_interest_selected_final"),
+  textOutput("language_selected"),
   DT::dataTableOutput('data_present'),
   p(),
   HTML("For instructions on how to use this app with your own Pokédex, please visit <a href='https://github.com/Peter-T-Ruehr/PokeChecker'  target='_blank'>PokeChecker's GitHub page.</a>"),
   p(),
-  HTML("Enjoy and cheers, <a href='https://x.com/Peter_Th_R'  target='_blank'>Pete</a>")
+  HTML("Enjoy and cheers, <a href='https://x.com/Peter_Th_R'  target='_blank'>Pete</a>"),
+  p(),
+  HTML("Unless otherwise stated, image content is taken from <a href='www.serebii.net/'  target='_blank'>www.serebii.net/</a>, licensed under the  <a href='https://creativecommons.org/licenses/by-nc-sa/2.5/'  target='_blank'>Creative Commons Attribution-NonCommercial-ShareAlike</a> license.")
+  
+  
 )
 
 # Define server logic
 server <- function(input, output, session) {
   # observe(language_column <- input$language)
   
-  # Make drop-down choice of sel_2 dependent upon user input of sel_1
+  # Make drop-down choice
   observeEvent(input$language, {
     updateSelectInput(session,
                       "mon_of_interest",
-                      choices = pokedex_df() %>% 
-                        pull(input$language)
+                      choices = pokedex_df_raw %>% 
+                        pull(input$language) # input$language
     )
   })
   
-  pokedex_df <- reactive({
-    read_sheet(input$pokedex_sheet_id, sheet = "pokedex") %>% 
-      rename(German = name_german,
-             English = name_english)
-  })
+  # # read pokedex sheet
+  #   pokedex_df <- reactive({
+  #   read_sheet(input$pokedex_sheet_id, sheet = "pokedex")
+  # })
+  # 
+  #   pokedex_df_raw <- pokedex_df()
   
+  # render table with present data
   output$data_present <- DT::renderDataTable({
     req(input$mon_of_interest,
         input$language)
@@ -77,200 +109,114 @@ server <- function(input, output, session) {
       paste("Original selection:", input$mon_of_interest)
     })
     
-    mon_of_interest <- input$mon_of_interest
-    
-    if(grepl("(Male)", mon_of_interest)){
-      suffix <- "(Male)"
-    } else if(grepl("(Female)", mon_of_interest)){
-      suffix <- "(Female)"
-    } else {
-      suffix <- ""
-    }
-    
-    # # testing
-    # mon_of_interest <- "Vivillon"
-    
-    if(grepl('\\(', mon_of_interest)){
-      mon_of_interest <- gsub(" \\(.+$", "", mon_of_interest)
-    }
-    
-    # translate from german to english
-    if(input$language == "German"){
-      mon_of_interest <- pokedex_df() %>% 
-        mutate(German = gsub(" \\(.+$", "", German)) %>% 
-        filter(German == mon_of_interest) %>% 
-        slice(1) %>% 
-        pull(English) 
-      
-      if(grepl('\\(', mon_of_interest)){
-        mon_of_interest <- gsub(" \\(.+$", "", mon_of_interest)
-      }
-    }
-    
-    if(suffix != ""){
-      mon_of_interest <- paste(mon_of_interest, suffix)
-    }
+    mon_of_interest <- gsub(" \\(Mega)", "", input$mon_of_interest) # str_split(
+    mon_of_interest <- gsub(" \\(Mega X\\)", "", mon_of_interest)
+    mon_of_interest <- gsub(" \\(Mega Y\\)", "", mon_of_interest)
     
     output$mon_of_interest_selected_final <- renderText({ 
       paste("Converted selection:", mon_of_interest)
     })
     
+    # testing
+    # mon_of_interest <- "Glurak"
+    mon_of_interest <- "Sandan"
+
+    # if(grepl("(Male)", mon_of_interest)){
+    #   suffix <- "(Male)"
+    # } else if(grepl("(Female)", mon_of_interest)){
+    #   suffix <- "(Female)"
+    # } else {
+    #   suffix <- ""
+    # }
+    
+    # curr_language <- "german"
+    
+    # mon_of_interest <- "Glumanda"
+    dex_of_interest <- translations_df %>%
+      # filter(language == curr_language) %>% 
+      filter(grepl(mon_of_interest, name)) %>% 
+      pull(dex)
+    
     stages <- evolutions_df %>% 
-      filter(name1 == mon_of_interest |
-               name2 == mon_of_interest |
-               name3 == mon_of_interest) %>% 
+      select(dex_A, dex_B, dex_C, dex_mega1, dex_mega2) %>% 
+      filter(dex_A == dex_of_interest |
+               dex_B == dex_of_interest |
+               dex_C == dex_of_interest) %>% 
       unlist()
     
-    stages <- unique(stages[!is.na(stages)])
+    # remove NAs from stages
+    stages <- tibble(dex = unique(stages[!is.na(stages)]))
     
+    # get info on catching status
+    stages_caught <- stages %>% 
+      left_join(pokedex_df_raw,# %>% 
+                # distinct(),
+                # select(-c(name_english, name_german)),
+                by = "dex") %>% 
+      drop_na(name_german)
     
-    # get entries of current stages
-    if(suffix == ""){
-    curr_pokedex_present <- pokedex_df() %>% 
-      mutate(English = gsub(" \\(.+", "", English)) %>% 
-      filter(English %in% stages)
-    } else{
-      curr_pokedex_present <- pokedex_df() %>% 
-        # mutate(English = gsub(" \\(.+", "", English)) %>% 
-        filter(English %in% stages)
-    }
-    
-    # fill alolan, hisuian, galaran , paldean shadow and purified
-    special_forms <- c("Alola", "Hisui", "Galar", "Paldea")
-    for(i in 1:nrow(curr_pokedex_present)){
-      for(form in special_forms){
-        if(sum(grepl(form, curr_pokedex_present$German))>=1){
-          rows_to_edit <- which(grepl(form, curr_pokedex_present$German))
-          row = 2
-          for(row in rows_to_edit){
-            curr_pokedex_present$shadow_in_dex[row] <- "(x)"
-            curr_pokedex_present$purified_in_dex[row] <- "(x)"
-          }
-        }
-      }
-    }
-    
-    # replace "-" with / for genders that don't exist
-    curr_pokedex_present <- curr_pokedex_present %>% 
-      mutate(male_in_dex = gsub("-", "/", male_in_dex),
-             female_in_dex = gsub("-", "/", female_in_dex))
-    
-    # add images to present entries
-    curr_pokedex_present <- curr_pokedex_present %>% 
-      replace(is.na(.), "-") %>% 
-      mutate(image = paste0("<img src=", "\"https://img.pokemondb.net/artwork/avif/", gsub("\\. ", "-", tolower(English)), ".avif \"",  "height=\"52\"></img>"))
-    
-    
-    # change to Alola images
-    if(sum(grepl("Alola", curr_pokedex_present$German)) >= 1){
-      for(i in 1:nrow(curr_pokedex_present)){
-        if(grepl("Alola", curr_pokedex_present$German[i])){
-          curr_pokedex_present$image[i] <- paste0("<img src=", "\"https://img.pokemondb.net/artwork/avif/", gsub("\\. ", "-", tolower(curr_pokedex_present$English[i])), "-alolan.avif \"",  "height=\"52\"></img>")
-          
-        }
-      }
-    }
-    
-    # change to Hisui images
-    if(sum(grepl("Hisui", curr_pokedex_present$German)) >= 1){
-      for(i in 1:nrow(curr_pokedex_present)){
-        if(grepl("Hisui", curr_pokedex_present$German[i])){
-          curr_pokedex_present$image[i] <- paste0("<img src=", "\"https://img.pokemondb.net/artwork/avif/", gsub("\\. ", "-", tolower(curr_pokedex_present$English[i])), "-hisuian.avif \"",  "height=\"52\"></img>")
-          
-        }
-      }
-    }
-    
-    # change to Galar images
-    if(sum(grepl("Galar", curr_pokedex_present$German)) >= 1){
-      for(i in 1:nrow(curr_pokedex_present)){
-        if(grepl("Galar", curr_pokedex_present$German[i])){
-          curr_pokedex_present$image[i] <- paste0("<img src=", "\"https://img.pokemondb.net/artwork/avif/", gsub("\\. ", "-", tolower(curr_pokedex_present$English[i])), "-galarian.avif \"",  "height=\"52\"></img>")
-          
-        }
-      }
-    }
-    
-    # change to Paldean images
-    if(sum(grepl("Paldea", curr_pokedex_present$German)) >= 1){
-      for(i in 1:nrow(curr_pokedex_present)){
-        if(grepl("Paldea", curr_pokedex_present$German[i])){
-          curr_pokedex_present$image[i] <- paste0("<img src=", "\"https://img.pokemondb.net/artwork/avif/", gsub("\\. ", "-", tolower(curr_pokedex_present$English[i])), "-paldean.avif \"",  "height=\"52\"></img>")
-          
-        }
-      }
-    }
-    
-    for(i in 1:nrow(curr_pokedex_present)){
-      curr_pokedex_present$image[i] <- gsub("mime jr..avif", "mime-jr.avif", curr_pokedex_present$image[i])
-      curr_pokedex_present$image[i] <- gsub("nidoran \\(male\\)", "nidoran-m", curr_pokedex_present$image[i])
-      curr_pokedex_present$image[i] <- gsub("nidoran \\(female\\)", "nidoran-f", curr_pokedex_present$image[i])
-      curr_pokedex_present$image[i] <- gsub("flabébé", "flabebe", curr_pokedex_present$image[i], fixed=TRUE)
-      if(curr_pokedex_present$German[i] == "Burmy (Plant)") curr_pokedex_present$image[i] <- gsub("burmy", "burmy-plant", curr_pokedex_present$image[i])
-      if(curr_pokedex_present$German[i] == "Burmy (Sandy)") curr_pokedex_present$image[i] <- gsub("burmy", "burmy-sandy", curr_pokedex_present$image[i])
-      if(curr_pokedex_present$German[i] == "Burmy (Trash)") curr_pokedex_present$image[i] <- gsub("burmy", "burmy-trash", curr_pokedex_present$image[i])
-      if(curr_pokedex_present$German[i] == "Burmadame (Plant)") curr_pokedex_present$image[i] <- gsub("wormadam", "wormadam-plant", curr_pokedex_present$image[i])
-      if(curr_pokedex_present$German[i] == "Burmadame (Sandy)") curr_pokedex_present$image[i] <- gsub("wormadam", "wormadam-sandy", curr_pokedex_present$image[i])
-      if(curr_pokedex_present$German[i] == "Burmadame (Trash)") curr_pokedex_present$image[i] <- gsub("wormadam", "wormadam-trash", curr_pokedex_present$image[i])
-      if(curr_pokedex_present$German[i] == "Moterpel (Plant)") curr_pokedex_present$image[i] <- gsub("mothim", "mothim-plant", curr_pokedex_present$image[i])
-      if(curr_pokedex_present$German[i] == "Moterpel (Sandy)") curr_pokedex_present$image[i] <- gsub("mothim", "mothim-sandy", curr_pokedex_present$image[i])
-      if(curr_pokedex_present$German[i] == "Moterpel (Trash)") curr_pokedex_present$image[i] <- gsub("mothim", "mothim-trash", curr_pokedex_present$image[i])
-    }
-    
+    # # here: think about how to deal with special forms and fill pokedex_df with entries
+    # # fill alolan, hisuian, galaran , paldean shadow and purified
+    # # get unique dexes
+    # dexes <- unique(stages_caught$dex)
+    # special_forms <- c("Alola", "Hisui", "Galar", "Paldea")
+    # 
     # i=1
-    for(i in 1:nrow(curr_pokedex_present)){
-      curr_pokedex_present[i,1:7] <- as.list(gsub("^x$", curr_pokedex_present[i,8], curr_pokedex_present[i,1:7]))
-      curr_pokedex_present[i,1:7] <- as.list(gsub("^b$", curr_pokedex_present[i,8], curr_pokedex_present[i,1:7]))
-    }
+    # j=4
+    # for(i in 1:length(dexes)){
+    #   curr_dex <- dexes[i]
+    #   curr_stages_caught <- stages_caught %>% 
+    #     filter(dex == curr_dex)
+    #   curr_name_english <- curr_stages_caught$name_english[i]
+    #   if(grepl(special_forms, curr_name_english))
+    #   for(j in 1:ncol(stages_caught)){
+    #     curr_cell <- stages_caught[i,j]
+    #   }
+    # }
+    # 
+    # form=special_forms[1]
+    # for(i in 1:nrow(stages_caught)){
+    #   for(form in special_forms){
+    #     if(sum(grepl(form, stages_caught$name_german))>=1){
+    #       rows_to_edit <- which(grepl(form, stages_caught$name_german))
+    #       row = 2
+    #       for(row in rows_to_edit){
+    #         stages_caught$shadow_in_dex[row] <- "x"
+    #         stages_caught$purified_in_dex[row] <- "x"
+    #       }
+    #     }
+    #   }
+    # }
     
-    curr_pokedex_present <- curr_pokedex_present %>% 
-      select(-image)
-    
-    colnames(curr_pokedex_present) <- gsub("_in_dex", "", colnames(curr_pokedex_present))
-    colnames(curr_pokedex_present) <- gsub("stars3", "`3 stars`", colnames(curr_pokedex_present))
-    curr_colnames <- colnames(curr_pokedex_present)
-    
-    curr_rownames <- rownames(t(curr_pokedex_present))
-    curr_pokedex_present <- tibble(as.data.frame(t(curr_pokedex_present)))
-    colnames(curr_pokedex_present) <- paste("stage", 1:ncol(curr_pokedex_present))
-    
-    if(ncol(curr_pokedex_present) == 3){
-      curr_pokedex_present <- curr_pokedex_present %>% 
-        mutate(var = curr_rownames) %>% 
-        select(var, `stage 1`:`stage 3`)
-    } else if(ncol(curr_pokedex_present) == 2){
-      curr_pokedex_present <- curr_pokedex_present %>% 
-        mutate(var = curr_rownames) %>% 
-        select(var, `stage 1`:`stage 2`)
-    } else if(ncol(curr_pokedex_present) == 1){
-      curr_pokedex_present <- curr_pokedex_present %>% 
-        mutate(var = curr_rownames) %>% 
-        select(var, `stage 1`)
-    }
-    
-    curr_pokedex_present <- curr_pokedex_present %>% 
-      mutate(var = curr_colnames) %>% 
-      select(var, 1:(ncol(curr_pokedex_present))) %>% 
-      mutate(var = gsub("name_german", "German", var),
-             var = gsub("name_english", "English", var))
-    
-    # separate stages if multple forms exist
-    if(grepl('\\(', curr_pokedex_present[1,2]) &
-       suffix == ""){
-      final_df <- tibble()
-      for(i in 1:length(stages)){
-        columns_with_curr_stage <- which(grepl(stages[i], curr_pokedex_present[2,]))
-        curr_df <- curr_pokedex_present[c(1,columns_with_curr_stage)]
-        colnames(curr_df) <- c("var", 1:(ncol(curr_df)-1))
-        final_df <- rbind(final_df,
-                          curr_df)
-        
+    # get images from https://serebii.net
+    i=1
+    j=5
+    for(i in 1:nrow(stages_caught)){
+      curr_dex <- stages_caught$dex[i]
+      curr_name_english <- stages_caught$name_english[i]
+      for(j in 1:ncol(stages_caught)){
+        stages_caught[i,j] <- gsub("^b$", "x", stages_caught[i,j])
+        if (grepl("Galarian", curr_name_english) == TRUE){
+          stages_caught[i,j] <- gsub("^x$", paste0("<img src=", 'https://www.serebii.net/pokemon/art/', 
+                                                   substring(curr_dex, first = 2), "-g.png \"",  "height=\"20\"></img>"), 
+                                     stages_caught[i,j])
+        } else if (grepl("Alolan", curr_name_english) == TRUE){
+          stages_caught[i,j] <- gsub("^x$", paste0("<img src=", 'https://www.serebii.net/pokemon/art/', 
+                                                   substring(curr_dex, first = 2), "-a.png \"",  "height=\"20\"></img>"), 
+                                     stages_caught[i,j])
+        } else {
+          stages_caught[i,j] <- gsub("^x$", paste0("<img src=", 'https://www.serebii.net/pokemon/art/', 
+                                                   substring(curr_dex, first = 2), ".png \"",  "height=\"20\"></img>"), 
+                                     stages_caught[i,j])
+        }
       }
-      curr_pokedex_present <- final_df
     }
     
     
-    DT::datatable(curr_pokedex_present,
+    
+    
+    # https://www.serebii.net/pokemon/art/144.png
+    DT::datatable(stages_caught,
                   escape = FALSE,
                   
                   # extensions = 'Buttons',
